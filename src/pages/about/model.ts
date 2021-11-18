bdc0f354153c8f5f7ccd1dff810d5063af21733f
xrei <romanrei.dev@gmail.com>
2021-11-19 01:43:50 +0300

    feat: about page

---
+++ src/pages/about/model.ts
@@ -0,0 +1,17 @@
+import {createEffect, createStore, forward} from 'effector'
+import {createGate} from 'effector-react'
+import {getAbout} from 'src/api'
+
+export const AboutPageGate = createGate('AboutPage')
+
+export const $about = createStore('')
+
+const fetchAboutData = createEffect<void, string>(async () => {
+  const resp = await getAbout()
+  const html = (await resp.json()) as string
+  return html.replaceAll('color: #333333; ', '')
+})
+
+forward({from: AboutPageGate.open, to: fetchAboutData})
+
+$about.on(fetchAboutData.doneData, (_, p) => p)
