be78f1988159a980ec86c70d5594f25daa71aefb
xrei <romanrei.dev@gmail.com>
2022-02-03 18:55:00 +0300

    add test button

--- src/features/Test/TestContent/ui/TestButton.tsx
+++ src/features/Test/TestContent/ui/TestButton.tsx
@@ -14,13 +14,24 @@ type TestButtonProps = {
 }
 
 export const TestButton = ({drawer}: TestButtonProps) => {
+  const currTheme = useStore(TestContentModel.$currentTheme)
   const hasTest = useStore(TestContentModel.$hasTestAndTheme)
   const hasUser = useStore(UserModel.$hasUser)
 
-  if (!hasUser) return <></>
+  if (!hasUser && !currTheme) return <></>
 
   if (hasTest) {
-    return <Button variant="outlined">Текущий тест</Button>
+    return (
+      <Button
+        component={Link}
+        to={routesPaths.tasks + `/${currTheme?.ThemeId}`}
+        variant="contained"
+        disableElevation
+        color={drawer ? 'primary' : 'success'}
+      >
+        Текущий тест
+      </Button>
+    )
   } else {
     return (
       <Button
@@ -41,4 +52,5 @@ const buttonCss = (drawer?: boolean) => (theme: any) =>
   css`
     width: 'auto';
     margin: ${drawer && theme.spacing(2)};
+    margin-top: ${drawer && theme.spacing(4)};
   `
