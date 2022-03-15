78cbc88ffb457405e808c3586fef5b74e13b4be9
xrei <romanrei.dev@gmail.com>
2022-03-15 16:20:03 +0300

    improve config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 9000,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://10.10.1.10:9999/sqltest/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({mode}) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+
+  const apiUrl = process.env.VITE_API_URL as string | undefined
+
+  return {
+    server: {
+      port: 9000,
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: resolve(__dirname, './src'),
+      },
     },
-  },
+  }
 })
