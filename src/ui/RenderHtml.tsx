21a87d6c4e2a4fc0ec737d6db6fb36470045d6db
xrei <romanrei.dev@gmail.com>
2022-03-21 19:20:09 +0300

    fix

--- src/ui/RenderHtml.tsx
+++ src/ui/RenderHtml.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import {CircularProgress, Box} from '@mui/material'
 
 type Props = {
   htmlStr: string
 }
 
 export const RenderHtml: React.FC<Props> = ({children, htmlStr}) => {
-  if (htmlStr) {
-    return <div dangerouslySetInnerHTML={{__html: htmlStr}}></div>
-  } else {
-    return (
-      <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
-        <CircularProgress />
-      </Box>
-    )
-  }
+  return <div dangerouslySetInnerHTML={{__html: htmlStr}}></div>
 }
